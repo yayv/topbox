528e70f885e212e06a94f23d583062ddf4f69995
liu ce <yayv.cn@gmail.com>
2016-06-30 18:05:20 +0800

    增加忽略目录的控制

---
+++ topbox/v/_run/95b9b881f465d6d37daca85f9dc9783915af60df_0.file.404.html.php
@@ -0,0 +1,36 @@
+<?php /* Smarty version 3.1.27, created on 2016-06-30 06:14:42
+         compiled from "v\manage\error_pages\404.html" */ ?>
+<?php
+/*%%SmartyHeaderCode:261265774b8d2d31491_45571358%%*/
+if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '95b9b881f465d6d37daca85f9dc9783915af60df' => 
+    array (
+      0 => 'v\\manage\\error_pages\\404.html',
+      1 => 1399230760,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '261265774b8d2d31491_45571358',
+  'has_nocache_code' => false,
+  'version' => '3.1.27',
+  'unifunc' => 'content_5774b8d2d48b98_48428845',
+),false);
+/*/%%SmartyHeaderCode%%*/
+if ($_valid && !is_callable('content_5774b8d2d48b98_48428845')) {
+function content_5774b8d2d48b98_48428845 ($_smarty_tpl) {
+
+$_smarty_tpl->properties['nocache_hash'] = '261265774b8d2d31491_45571358';
+?>
+<!DOCTYPE HTML>
+<HTML>
+	<head>
+	</head>
+	<body>
+		404 Page Not Found !
+	</body>
+</HTML><?php }
+}
+?>
